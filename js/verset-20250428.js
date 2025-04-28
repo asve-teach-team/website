b3c095d2172da426125b65e29263f04dbccb73cd
Mohamed El Ouadi <melouadi@asve-vaureal.fr>
2025-04-28 17:52:04 +0200

    force reload js by skipping browser cache

--- js/verset-20250428.js
+++ js/verset-20250428.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
             verseResult.innerHTML =
                 '<div class="loading">Chargement du verset...</div>'; // Message de chargement
             // take only verse by subject - see https://www.alislam.org/quran/app/topics-a - use (verseNumber - 1) - topic 'arabic'
-            const arabicVerses = ["12/2", "13/37", "16/103", "20/113", "26/195", "39/28", "41/3", "41/44", "42/7", "43/3", "46/12", "50/30"];
+            const arabicVerses = ["12/2", "13/37", "16/103", "20/113", "26/195", "39/28", "41/3", "41/44", "42/7", "43/3", "46/12"];
             const verse = arabicVerses[Math.floor(Math.random() * arabicVerses.length)];
             const url = "https://api.asve-vaureal.fr/quran/v1/verse/" + verse; // Lien pour obtenir un verset aléatoire
             const response = await fetch(url);
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
         verseResult.innerHTML = `
         <div class="card mx-auto shadow-lg">
             <div class="card-body">
-                <h3 class="text-gradient">Réflexion Spirituelle : Verset Aléatoire (${verse.surat}:${verse.verse})</h3>
-                <img class="card-text verset-text" src="${verse.picture}" />
+                <h3 class="card-title">Réflexion Spirituelle : Verset Aléatoire (${verse.surat}:${verse.verse})</h3>
+                <img class="card-text verset-text" src="${verse.image}" />
                 <p class="card-text p-ayah-fr">${verse.text.fr}</p>
             </div> 
         </div>`;
