c142793f246585c3f8ec29c2abbd429506641750
Mohamed El Ouadi <melouadi@asve-vaureal.fr>
2025-04-30 21:08:35 +0200

    add sample connected service

--- v2/js/service.js
+++ v2/js/service.js
@@ -2,7 +2,8 @@ async function getSchool() {
     let apiResult = document.getElementById("apiResult");
     apiResult.innerHTML = "Calling ...";
     const response = await fetch("https://api.asve-vaureal.fr/school/v1/school", {
-        credentials: "same-origin"
+        method: 'GET',
+        credentials: 'include'
     });
     if (!response.ok) {
         apiResult.innerHTML = `<div class="alert alert-danger">${response.status}</div>`
