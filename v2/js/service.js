75968db23bc03910705fa83b0b95dfbaece4062f
Mohamed El Ouadi <melouadi@asve-vaureal.fr>
2025-04-30 21:22:58 +0200

    add sample connected service

--- v2/js/service.js
+++ v2/js/service.js
@@ -1,14 +1,22 @@
 async function getSchool() {
     let apiResult = document.getElementById("apiResult");
     apiResult.innerHTML = "Calling ...";
-    const response = await fetch("https://api.asve-vaureal.fr/school/v1/school", {
-        method: 'GET',
-        credentials: 'include'
-    });
-    if (!response.ok) {
-        apiResult.innerHTML = `<div class="alert alert-danger">${response.status}</div>`
-    } else {
+    try {
+        const response = await fetch("https://api.asve-vaureal.fr/school/v1/school", {
+            headers: {
+                'Cookie': getCookieValue("asve-vaureal-cookie")
+            }
+        });
+        if (!response.ok) {
+            throw new Error(
+                `Response status: ${response.status}`
+            );
+        }
         const resp = await response.json();
         apiResult.textContent = JSON.stringify(resp, undefined, 2);
+    }  catch (e) {
+        apiResult.innerHTML = `<div class="alert alert-danger">${e.message}</div>`
     }
+
+
 }
